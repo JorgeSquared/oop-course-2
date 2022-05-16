a5f68747b9bf3d5a673f00a4ccfaec084ed7b1e8
jirka <jorgetimes2@gmail.com>
2022-05-16 05:22:31 +0200

    Container: Force Single Objects, Celebrate

--- lib/Container.php
+++ lib/Container.php
@@ -4,6 +4,12 @@ class Container
 {
     private $configuration;
 
+    private $pdo;
+
+    private $shipLoader;
+
+    private $battleManager;
+
     public function __construct(array $configuration)
     {
         $this->configuration = $configuration;
@@ -11,12 +17,33 @@ class Container
 
     public function getPDO(): PDO
     {
-        $pdo = new PDO(
-            $this->configuration['db_dsn'],
-            $this->configuration['db_user'],
-            $this->configuration['db_pass']
-        );
+        if ($this->pdo === null) {
+            $this->pdo = new PDO(
+                $this->configuration['db_dsn'],
+                $this->configuration['db_user'],
+                $this->configuration['db_pass']
+            );
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
 
-        return $pdo;
+        return $this->pdo;
+    }
+
+    public function getShipLoader(): ShipLoader
+    {
+        if ($this->shipLoader === null) {
+            $this->shipLoader = new ShipLoader($this->getPDO());
+        }
+
+        return $this->shipLoader;
+    }
+
+    public function getBattleManager()
+    {
+        if ($this->battleManager === null) {
+            $this->battleManager = new BattleManager();
+        }
+
+        return $this->battleManager;
     }
 }
